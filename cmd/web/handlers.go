166b16df64984f664aafa3bc8166f334b10c5ce9
0x-sudo <rahul_chaudhary@tutanota.com>
2024-03-03 09:23:14 +0530

    chore: executing sql statements

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -45,10 +45,19 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		// w.WriteHeader(http.StatusMethodNotAllowed)
-		// w.Write([]byte(http.StatusText(http.StatusMethodNotAllowed)))
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create a new snippet..."))
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
